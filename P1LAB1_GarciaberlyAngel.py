fb35df0342ad95e58b12cb9d497c3cd7ab52dc40
AngelBerly1 <166738184+AngelBerly1@users.noreply.github.com>
2024-04-14 23:54:36 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_GarciaberlyAngel.py
@@ -0,0 +1,8 @@
+#AngelGarcia
+#4/9/24
+#Plab1
+#Creating a introduction to python
+firstname=input("Enter your first name:")
+lastname=input("Enter your last name:")
+fullname= firstname+" "+ lastname
+print(f"Hello,{firstname} {lastname} !Welcome to CTI-110")
